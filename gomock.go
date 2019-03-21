52b3aeb19d16254211a668a76ef11183fcd18aa9
wgxue <wgxue@ctrip.com>
2019-03-21 13:11:17 +0800

    pprof

--- gomock.go
+++ gomock.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/pprof"
 	"main/api"
 )
 
@@ -27,6 +28,16 @@ func main() {
 	app.Get("admin/mock/get", func(ctx iris.Context) {
 		ctx.JSON(house.GetAllApi())
 	})
+	app.Get("/debug", func(ctx iris.Context) {
+		ctx.HTML("<h1><a href='/debug/pprof/heap'>heap</a><br>" +
+			"<a href='/debug/pprof/goroutine'>goroutine</a><br>" +
+			"<a href='/debug/pprof/debug/block'>block</a><br>" +
+			"<a href='/debug/pprof/debug/cmdline'>cmdline</a><br>" +
+			"<a href='/debug/pprof/debug/profile'>profile</a><br>" +
+			"<a href='/debug/pprof/debug/symbol'>symbol</a><br>" +
+			"<a href='/debug/pprof/threadcreate'>threadcreate</a><br>")
+	})
+	app.Any("/debug/pprof/{action:path}", pprof.New())
 	go app.Run(iris.Addr(":" + *adminPort))
 	house.Start(*workingPort)
 }
